aeed9db8f836705272fb4efd9815cc2da1f5a76f
herjean <wong0786@e.ntu.edu.sg>
2016-07-06 02:04:59 +0800

    Added in Speech Recognition Engine

--- VoiceCommands/Form1.Designer.cs
+++ VoiceCommands/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listenBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listenBtn
+            // 
+            this.listenBtn.Location = new System.Drawing.Point(89, 135);
+            this.listenBtn.Name = "listenBtn";
+            this.listenBtn.Size = new System.Drawing.Size(109, 23);
+            this.listenBtn.TabIndex = 0;
+            this.listenBtn.Text = "Start Listening";
+            this.listenBtn.UseVisualStyleBackColor = true;
+            this.listenBtn.Click += new System.EventHandler(this.listenBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.listenBtn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button listenBtn;
     }
 }
 
--- VoiceCommands/Form1.cs
+++ VoiceCommands/Form1.cs
@@ -1,12 +1,82 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
 
 namespace VoiceCommands
 {
     public partial class Form1 : Form
     {
+
+        #region Variable Declaration
+
+        SpeechSynthesizer ss = new SpeechSynthesizer();
+        PromptBuilder pb = new PromptBuilder();
+        SpeechRecognitionEngine engine = new SpeechRecognitionEngine();
+
+        #endregion
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        [DllImport("user32")]
+        public static extern void
+        LockWorkStation();
+
+        private void engine_SpeechRecognised(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text.ToString())
+            {
+                case "hello":
+                    ss.SpeakAsync("hello handsome");
+                    break;
+
+                case "what is the current time":
+                    ss.SpeakAsync("current time is " + DateTime.Now.ToLongTimeString());
+                    break;
+
+                case "how are you":
+                    ss.SpeakAsync("i'm feeling great today");
+                    break;
+
+                case "shut down":
+                    Process.Start("shutdown", "/s /t 0");
+                    break;
+
+                case "thank you":
+                    ss.SpeakAsync("pleasure is mine");
+                    break;
+
+                case "protect":
+                    LockWorkStation();
+                    break;
+            }
+        }
+
+        private void listenBtn_Click(object sender, EventArgs e)
+        {
+            listenBtn.Enabled = false;
+            Choices clist = new Choices();
+            string[] keyWordList = { "hello", "how are you", "what is the current time", "shut down", "thank you", "protect", "open edge" };
+            clist.Add(keyWordList);
+            Grammar gr = new Grammar(new GrammarBuilder(clist));
+
+            try
+            {
+                engine.RequestRecognizerUpdate();
+                engine.LoadGrammar(gr);
+                engine.SpeechRecognized += engine_SpeechRecognised;
+                engine.SetInputToDefaultAudioDevice();
+                engine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
         }
     }
 }
